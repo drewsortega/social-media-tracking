1579877a294de0b58314d3915b63c70b96ed0ff9
Drew S. Ortega <DrewSOrtega@gmail.com>
2019-05-23 22:10:10 -0700

    get basic extension running

--- src/background.js
+++ src/background.js
@@ -1,4 +1,6 @@
+//Set up background event listener
 chrome.runtime.onInstalled.addListener(function () {
+    //example event
     chrome.storage.sync.set({ color: '#3aa757' }, function () {
         console.log("The color is green.");
     });
