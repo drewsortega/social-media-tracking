2d5387f7cd427af414375ba122ff3008607dddd6
Kamille Delgardo <artemis@pop-os.localdomain>
2019-05-30 16:53:50 -0700

    lay out tests

--- tests/test_suite.js
+++ tests/test_suite.js
@@ -1,5 +1,20 @@
-describe("A suite", function() {
-  it("contains spec with an expectation", function() {
+describe("Google OAuth", function() {
+  it("directs user to Google login page upon button click", function() {
+    expect(true).toBe(true);
+  });
+  it("receives OAuth token", function() {
+    expect(true).toBe(true);
+  });
+  it("stores OAuth token", function() {
+    expect(true).toBe(true);
+  });
+  it("accepts Google OAuth callback URL messages for successful login", function() {
+    expect(true).toBe(true);
+  });
+  it("accepts Google OAuth callback URL messages for failed login", function() {
+    expect(true).toBe(true);
+  });
+  it("returns user to login screen when OAuth token expires", function() {
     expect(true).toBe(true);
   });
 });
