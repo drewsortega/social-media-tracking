106835da0138bc80e394f5688424a9ef2b66ea90
Drew S. Ortega <DrewSOrtega@gmail.com>
2019-06-04 23:21:28 -0700

    added non-working sql query code, will fix when using good ip address

--- server/server.js
+++ server/server.js
@@ -32,37 +32,57 @@ app.use(bodyParser.json());
 
 app.post('/auth/login_signup', (req, res) => {
     console.log(req.body);
+    let user = {};
     Bluebird.try(() => {
         if (req.body && !_.isNull(req.body.id) && _.isNumber(+req.body.id)
             && !_.isNull(req.body.full_name) && _.isString(req.body.full_name)
-            && !_.isNull(req.body.email) && +_.isString(req.body.email)
+            && !_.isNull(req.body.email) && _.isString(req.body.email)
+            && !_.isNull(req.body.token) && _.isString(req.body.token)
         ) {
-            let request = new Request("select * from [dbo].[cs361_project]");
+            let request = new Request(`select * from [dbo].[cs361_project] where id=${req.body.id}`);
             connection.execSql(request);
             return request;
         } else {
-            throw new Error("Malformed post");
+            Bluebird.reject("Malformed post");
         }
     }).then((rowCount, rows) => {
         console.log(rowCount + ' rows');
         console.log(rows);
+        let query = '';
+        if (rowCount == 1) {
+            user = rows[0];
+            query += `update [dbo].[cs361_project] set user_token=${req.body.token} where id=${req.body.id}`;
+        } else if (rowCount == 0) {
+            query += `insert into [dbo].[cs361_project] (FIELDS HERE) values (VALUES HERE)`
+        } else if (rowCount > 1) {
+            Bluebird.reject("Too many users");
+        }
+        let request = new Request(query);
+        connection.execSql(request);
         res.json(rows);
+    }).then((rowCount, rows) => {
+        if (rowCount == 1) {
+            res.status(200).send("successfully added/updated user");
+        } else {
+            res.status(400).send("failed to add/update user, server failure");
+        }
     }).catch((err) => {
         console.log(err);
-        res.status(401);
-        res.send("error");
+        res.status(401).send(err);
     }).finally(() => {
-    })
+        res.status(401).send("unknown error");
+    });
 });
 
 connection.on('connect', function (err) {
     if (err) {
         console.log("FATAL: Failed to connect: " + err);
-        process.exit(1);
+        //process.exit(1);
     } else {
         console.log("Connected to database.");
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`);
-        });
     }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
