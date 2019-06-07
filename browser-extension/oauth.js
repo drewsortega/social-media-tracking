51fbb5a94a4aa6390984125dc0280f0471b5b505
Drew S. Ortega <DrewSOrtega@gmail.com>
2019-06-06 22:22:04 -0700

    add call to server backend from frontend, completing Google Login Retention story

--- browser-extension/oauth.js
+++ browser-extension/oauth.js
@@ -2,7 +2,7 @@ var weekFromNow = Date.now() + 604800000
 
 window.onload = function () {
 
-    chrome.identity.getAuthToken({interactive: false }, function (token) {
+    chrome.identity.getAuthToken({ interactive: false }, function (token) {
         if (token != undefined) {
             console.log("logged in!");
         }
@@ -14,7 +14,31 @@ window.onload = function () {
             if (token != undefined) {
                 // Handle login success case.
                 console.log("login successful: " + token);
-                chrome.browserAction.setPopup({popup: 'dashboard.html'});
+                chrome.identity.getProfileUserInfo((profile) => {
+                    var profileRequest = new XMLHttpRequest();
+                    profileRequest.open('GET', 'https://www.googleapis.com/userinfo/v2/me');
+                    profileRequest.setRequestHeader('Content-length', 0);
+                    profileRequest.setRequestHeader('Authorization', `Bearer ${token}`);
+                    profileRequest.onload = () => {
+                        let userInfo = JSON.parse(profileRequest.responseText);
+                        let storeData = {
+                            id: userInfo.id,
+                            full_name: userInfo.name,
+                            given_name: userInfo.given_name,
+                            image_url: userInfo.image,
+                            token
+                        };
+                        let storeRequest = new XMLHttpRequest();
+                        storeRequest.open('POST', 'http://flip1.engr.oregonstate.edu:35432/auth/login_signup');
+                        storeRequest.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+                        storeRequest.onload = () => {
+                            console.log('saved user profile data');
+                        }
+                        storeRequest.send(JSON.stringify(storeData));
+                    };
+                    profileRequest.send();
+                });
+                chrome.browserAction.setPopup({ popup: 'dashboard.html' });
             } else {
                 // Handle login failure case.
             }
@@ -23,11 +47,11 @@ window.onload = function () {
     })
 
     document.getElementById('logout').addEventListener('click', function () {
-        chrome.identity.getAuthToken({interactive: false }, function (token) {
+        chrome.identity.getAuthToken({ interactive: false }, function (token) {
             if (token != undefined) {
-                chrome.identity.removeCachedAuthToken({token: token});
+                chrome.identity.removeCachedAuthToken({ token: token });
                 // FIXME: This should probably be handled globally, in background.js, using onSignInChanged().
-                chrome.browserAction.setPopup({popup: ''});
+                chrome.browserAction.setPopup({ popup: '' });
             }
         });
     })
