51d564c6660aaa3cf74c132046df2fe04fa264b3
Jon Atkinson <atkinsoj@oregonstate.edu>
2019-06-07 03:01:56 -0700

    Removed unneeded placeholder tests.

--- browser-extension/tests/test_suite.js
+++ browser-extension/tests/test_suite.js
@@ -1,22 +1,4 @@
 describe("Google OAuth", function() {
-  it("directs user to Google login page upon button click", function() {
-    expect(true).toBe(true);
-  });
-  it("receives OAuth token", function() {
-    expect(true).toBe(true);
-  });
-  it("stores OAuth token", function() {
-    expect(true).toBe(true);
-  });
-  it("accepts Google OAuth callback URL messages for successful login", function() {
-    expect(true).toBe(true);
-  });
-  it("accepts Google OAuth callback URL messages for failed login", function() {
-    expect(true).toBe(true);
-  });
-  it("returns user to login screen when OAuth token expires", function() {
-    expect(true).toBe(true);
-  });
 
   it("logs out the user when the Logout button is clicked on the dashboard", function() {
 
