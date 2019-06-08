cef34f609f2a8677d461433ae011d2a0cccc6b82
Jon Atkinson <atkinsoj@oregonstate.edu>
2019-06-08 00:00:06 -0700

    Whoops, forgot to commit my unit test

--- server/test/test.js
+++ server/test/test.js
@@ -1,6 +1,8 @@
 let chai = require('chai');
 let expect = chai.expect;
 
+let request = require('request');
+
 describe('Header', function () {
     describe('sub header', function () {
         it('should compare two equal strings', function () {
@@ -8,3 +10,21 @@ describe('Header', function () {
         })
     })
 })
+
+describe('server', function() {
+
+    var server_url = "http://localhost:54102/auth/login_retrieve/";
+
+    describe('auth', function() {
+        describe('login_retrieve', function() {
+
+            it('rejects tokens with punctuation', function(done) {
+                
+                request(server_url + '12345$1', function(error, response, body) {
+                    expect(response.statusCode).to.equal(401);
+                    done();
+                });
+            });
+        });
+    });
+});
